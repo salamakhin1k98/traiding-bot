e684a53cb7e316648dd52169bf8b197c66759ed6
salamakhin1k98 <58666932+salamakhin1k98@users.noreply.github.com>
2021-06-10 19:08:09 +0300

    MR-1 Записывать в базу отправленные сообщения

--- src/main/java/traiding/bot/entity/Message.java
+++ src/main/java/traiding/bot/entity/Message.java
@@ -4,17 +4,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "messages")
 @NoArgsConstructor
 public class Message {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "m_id")
     private String chatId;
 
-    private String text;
-
+    @Column(name = "m_date")
     private Date date = new Date();
+
+    @Column(name = "m_message")
+    private String text;
 
 }
