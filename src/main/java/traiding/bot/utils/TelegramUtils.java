fcfba39dad215808f9b3c27535549fa31db2e04f
salamakhin1k98 <58666932+salamakhin1k98@users.noreply.github.com>
2021-06-10 19:22:55 +0300

    MR-2 Изменить отправляемые сообщение для читабельности

--- src/main/java/traiding/bot/utils/TelegramUtils.java
+++ src/main/java/traiding/bot/utils/TelegramUtils.java
@@ -3,6 +3,7 @@ package traiding.bot.utils;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 import traiding.bot.entity.Signal;
+import traiding.bot.entity.enums.TypeOperation;
 import traiding.bot.telegram.enums.DictionaryTelegram;
 
 @NoArgsConstructor
@@ -14,8 +15,9 @@ public class TelegramUtils {
     }
 
     public String convertToMessage(Signal signal) {
-        return String.format("Временной промежуток : %s\nОперация : %s\nМетод : %s\nВалюта : %s\nЦена закрытия : %s",
-               signal.getTimeFrame().getType(), signal.getTypeOperation().getType(), signal.getMethodType().getType(), signal.getCurrencyName(), signal.getCurrencyValue());
+        String tag = signal.getTypeOperation().equals(TypeOperation.BUY) ? "U+2705" : "U+274C";
+        return String.format("%s\nВременной промежуток : %s\nОперация : %s\nМетод : %s\nВалюта : %s\nЦена закрытия : %s",
+               tag, signal.getTimeFrame().getType(), signal.getTypeOperation().getType(), signal.getMethodType().getType(), signal.getCurrencyName(), signal.getCurrencyValue());
     }
 
 }
